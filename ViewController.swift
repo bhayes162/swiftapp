008d2cb934e1c1568484bb9a780463cdf8860f24
Bryan Hayes <66hayes@gmail.com>
2017-09-17 15:18:55 -0400

    boolean learning with addition

--- ViewController.swift
+++ ViewController.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var coolLabel: UILabel!
-
+    
     
     @IBOutlet weak var text1: UITextField!
     
@@ -21,19 +21,27 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-        coolLabel.text = "\(Double(text1.text!)! + Double(text2.text!)!)"
+        let addition = false
+        
+        if addition {
+            coolLabel.text = "\(Double(text1.text!)! + Double(text2.text!)!)"
+        } else {
+            coolLabel.text = "\(Double(text1.text!)! - Double(text2.text!)!)"
+            
         }
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
